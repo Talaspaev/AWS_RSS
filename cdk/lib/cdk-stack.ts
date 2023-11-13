eefd0a5fdfd8f0cde7897645d7241b16da88275c
Mukhamed-Talaspaev <mukhamed.talaspayev@gmail.com>
2023-11-14 00:18:03 +0300

    task-2

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { StaticSite } from '../static-site';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    new StaticSite(this,'MyFirstBucket')
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
